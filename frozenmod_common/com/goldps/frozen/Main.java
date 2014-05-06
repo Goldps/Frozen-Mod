e197beca1094d67f418848d3f96257b1d21b4fd0
Goldps <josh12josh@yahoo.com>
2014-05-05 21:06:51 -0400

    First Commit

---
+++ frozenmod_common/com/goldps/frozen/Main.java
@@ -0,0 +1,20 @@
+package com.goldps.frozen;
+
+import com.goldps.lib.References;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = References.MODID, version = References.MODVERSION, name = References.MODNAME)
+public class Main
+{
+	
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
